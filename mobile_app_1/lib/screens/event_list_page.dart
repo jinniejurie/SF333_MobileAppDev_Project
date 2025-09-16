b3b2ef3d9dc6f729108313653c94ed004a41a490
mynt <patthananmint.v@gmail.com>
2025-09-16 14:40:05 +0700

    update event list

--- mobile_app_1/lib/screens/event_list_page.dart
+++ mobile_app_1/lib/screens/event_list_page.dart
@@ -1,13 +1,15 @@
 // lib/screens/event_list_page.dart
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'event_detail_page.dart';
 
 class EventListPage extends StatelessWidget {
-  final String currentUserId = "uid1"; // Dummy user
+  const EventListPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final String currentUserId = FirebaseAuth.instance.currentUser?.uid ?? '';
     return Scaffold(
       appBar: AppBar(
         title: const Text('Event List'),
